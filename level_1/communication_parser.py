4ae179b8644df08a4ba8bd6c3af00aafe709167c
KAYWeiss <weiss.kevin@free.fr>
2021-11-29 17:08:44 +0100

    refacto level_1 class and config

--- level_1/communication_parser.py
+++ level_1/communication_parser.py
@@ -1,46 +1,69 @@
 import ast 
 import json
 
+class CommunicationLogParser:
+    def __init__(self, cfg):
+        self.config = cfg
 
-def parse(communication_log):
-    """ 
-    This function takes a string as input and transforms it to dict
-    """
-    communication_log_informations = communication_log.split("|")
-    communication_dict = {}
-    for inf in communication_log_informations:
-        k, v = inf.split("=")
-        if k == 'sender':
-            v_dict = ast.literal_eval(v)
-            rename_key('profession', 'category', v_dict)
-            communication_dict[k] = v_dict
-        else:
-            communication_dict[k] = v
-    return communication_dict
+    @classmethod
+    def init_from_config_file(cls, config_file_path):
+        with open(config_file_path, 'r') as f:
+            config = json.load(f)
+            return cls(config)
 
-def rename_key(old_name, new_name, dict_to_change):
-    """ 
-    This function takes as 2 strings and a dict and returns a new dict with a key renamed
-    """
-    if old_name in dict_to_change.keys():
-        dict_to_change[new_name] = dict_to_change.pop(old_name)
-    else:
-        dict_to_change
 
-def communication_file_read(input_file_path):
-    """ 
-    This function opens a file andread and returns the first line as a string.
-    """
-    with open(input_file_path,'r') as input_file:
-        return input_file.readline()
+    def parse(self, communication_log):
+        """ 
+        This function takes a string as input and transforms it to dict.
+        It uses the input config to split the original string, and transform the different keys.
+        """
+        input_config = self.config.get('input')
+        communication_log_informations = communication_log.split(input_config.get("log_separator"))
+        communication_dict = {}
+        for inf in communication_log_informations:
+            k, v = inf.split(input_config.get("kv_separator"))
+            if k in input_config.get('dict_keys'):
+                v_dict = ast.literal_eval(v)
+                communication_dict[k] = v_dict
+            else:
+                communication_dict[k] = v
+        
+        self.rename_keys(communication_dict)
+        return communication_dict
 
-def communication_json_write(output_file_path, communication_log):
-    """ 
-    This function write a dict to a json file
-    """
-    with open(output_file_path, 'w') as output_file:
-        output_file.writelines(
-            json.dumps(communication_log, indent = 4, separators=(',',':'))
-                .replace('\"', '\'')
-        )
+    def communication_json_write(self, output_file_path, communication_log):
+        """ 
+        This function write a dict to a json file.
+        It uses the config to define the indent, separatorsand quotes of the json output. 
+        """
+        output_config = self.config.get('output')
+        output_string = json.dumps(communication_log, 
+                indent = output_config.get('indent'), 
+                separators = output_config.get('separators'))
+        if output_config.get('double_quotes_to_single') == 'True':
+            output_string = output_string.replace('\"', '\'')
+
+        with open(output_file_path, 'w') as output_file:
+            output_file.writelines(output_string)
+
+    
+    def rename_keys(self, dict_to_change):
+        """ 
+        This function takes as 2 strings and a dict and returns a new dict with a key renamed
+        """
+        changed_dict = dict_to_change
+        for old_key, new_key in self.config.get('keys_to_rename').items():
+            nested_dict = changed_dict
+            for nested_key in old_key.split('.')[:-1]:
+                nested_dict = nested_dict.get(nested_key)
+            nested_dict[new_key] = nested_dict.pop(old_key.split('.')[-1])
+        return changed_dict
+
+
+    def communication_file_read(input_file_path):
+        """ 
+        This function opens a file andread and returns the first line as a string.
+        """
+        with open(input_file_path,'r') as input_file:
+            return input_file.readline()
 
--- tests/test_level_1.py
+++ tests/test_level_1.py
@@ -1,21 +1,9 @@
 import ast
-from level_1.communication_parser import parse, communication_json_write
-
-def test_communication():
-    communication_sample = "id=testId|telecom=mail|created_at=1993-03-11 04:19:12|sender={'name': 'Kallum Wises', 'profession': 'writer'}"
-    
-    processed_communication_sample = {
-   'id':'testId',
-   'telecom':'mail',
-   'created_at':'1993-03-11 04:19:12',
-   'sender':{
-      'name':'Kallum Wises',
-      'category':'writer'
-        }
-    }
-    assert parse(communication_sample) == processed_communication_sample
+from level_1.communication_parser import CommunicationLogParser
 
 def test_writing():
+    communication_log_parser = CommunicationLogParser.init_from_config_file('./level_1/communication_config.json')
+
     output_path = "./tests/test_output.json"
     processed_communication_sample = {
    'id':'testId',
@@ -26,9 +14,50 @@ def test_writing():
       'category':'writer'
         }
     }
-    communication_json_write(output_path, processed_communication_sample)
+    communication_log_parser.communication_json_write(output_path, processed_communication_sample)
 
 def test_read_json_output():
     input_path = "./tests/test_output.json"
     with open(input_path, 'r') as input_file:
         print(ast.literal_eval(' '.join(map(lambda s: s.replace('\n',''), input_file.readlines()))))
+
+
+def test_should_rename_dict_keys():
+    communication_log_parser = CommunicationLogParser.init_from_config_file('./level_1/communication_config.json')
+    communication_log_parser.config["keys_to_rename"]['telecom'] = 'communication_mean'
+    communication_sample_before_renaming = {
+   'id':'testId',
+   'telecom':'mail',
+   'created_at':'1993-03-11 04:19:12',
+   'sender':{
+      'name':'Kallum Wises',
+      'profession':'writer'
+        }
+    }
+
+    expected_communication_sample = {
+   'id':'testId',
+   'communication_mean':'mail',
+   'created_at':'1993-03-11 04:19:12',
+   'sender':{
+      'name':'Kallum Wises',
+      'category':'writer'
+        }
+    }
+
+    assert(communication_log_parser.rename_keys(communication_sample_before_renaming) == expected_communication_sample)
+    
+def test_communication_parsing():
+    communication_log_parser = CommunicationLogParser.load_config('./level_1/communication_config.json')
+    communication_sample = "id=testId|telecom=mail|created_at=1993-03-11 04:19:12|sender={'name': 'Kallum Wises', 'profession': 'writer'}"
+    
+    expected_communication_sample = {
+   'id':'testId',
+   'telecom':'mail',
+   'created_at':'1993-03-11 04:19:12',
+   'sender':{
+      'name':'Kallum Wises',
+      'category':'writer'
+        }
+    }
+    assert communication_log_parser.parse(communication_sample) == expected_communication_sample
